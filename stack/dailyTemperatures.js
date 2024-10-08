d713ef48ec7523ef0fb39e70b947fe37023aa84a
shuvoedward <38678545+shuvoedward@users.noreply.github.com>
2024-10-08 18:59:38 -0400

    added soluion code

--- stack/dailyTemperatures.js
+++ stack/dailyTemperatures.js
@@ -11,17 +11,24 @@ function dailyTemperatures(temp) {
     }
     return result;
 }
-console.log(dailyTemperatures([30, 38, 30, 36, 35, 40, 28]));
-console.log(dailyTemperatures([22, 21, 20]));
+// console.log(dailyTemperatures([30, 38, 30, 36, 35, 40, 28]));
+// console.log(dailyTemperatures([22, 21, 20]));
 
-
-const solution = (temp) =>{
+const solution = (temp) => {
     const result = new Array(temp.length).fill(0);
     const stack = [];
 
-    for(let i = 0; i<temp.length; i++){
+    for (let i = 0; i < temp.length; i++) {
         const t = temp[i];
-        while(stack.length > 0 && )
-        stack.push([t,i]);
+        while (stack.length > 0 && stack[stack.length - 1][0] < t) {
+            const [stackT, stackIndex] = stack.pop();
+            result[stackIndex] = i - stackIndex;
+        }
+        stack.push([t, i]);
     }
-}
+    return result;
+    // Time complexity - O(n)
+    // Space complexity - O(n)
+};
+
+console.log(solution([30, 38, 30, 36, 35, 40, 28]));
