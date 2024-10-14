eb527a54203a057d96d4f6ca243a9109540c694e
shuvoedward <38678545+shuvoedward@users.noreply.github.com>
2024-10-13 21:12:51 -0400

    prefilled output array for faster array operations

--- arrayHashing/productsArray.js
+++ arrayHashing/productsArray.js
@@ -35,12 +35,12 @@ function multiplyByDivision(nums) {
 }
 
 function withoutDivision(nums) {
-    const output = [];
+    const output = new Array(nums.length).fill(1);
     let prefix = 1;
     let postfix = 1;
 
     for (let i = 0; i < nums.length; i++) {
-        output.push(prefix);
+        output[i] = prefix;
         prefix *= nums[i];
     }
     for (let i = nums.length - 1; i >= 0; i--) {
