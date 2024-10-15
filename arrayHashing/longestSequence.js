b88b47fa6fc460ce0661c648b88b38b2dfa6bdaa
shuvoedward <38678545+shuvoedward@users.noreply.github.com>
2024-10-14 21:31:11 -0400

    updated longest sequence code

--- arrayHashing/longestSequence.js
+++ arrayHashing/longestSequence.js
@@ -4,18 +4,6 @@ nums = [2, 20, 4, 10, 3, 4, 5];
 
 // how to find consecutive sequence?
 
-function longestConsecutive(nums) {
-    const graph = new Map();
-    const stack = [];
-    for (let number of nums) {
-        graph.set(number, [number - 1, number + 1]);
-    }
-
-    for (let value of graph) {
-        while (stack.length > 0) {}
-    }
-}
-
 // TODO: instead of going to the solution, from solution find the process
 function solution(nums) {
     const set = new Set(nums);
