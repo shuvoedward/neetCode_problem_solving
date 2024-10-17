6a10909bcdcc07555fc7968a3b5f09eed986f844
shuvoedward <38678545+shuvoedward@users.noreply.github.com>
2024-10-16 21:28:35 -0400

    three int sum solution
    This solution uses two pointer principles

--- twoPointer/threeIntSum.js
+++ twoPointer/threeIntSum.js
@@ -1,3 +1,39 @@
 function threeSum(nums) {
     nums.sort((a, b) => a - b);
+    const result = [];
+
+    let i = 0;
+    while (i < nums.length - 3) {
+        if (nums[i] === 0) {
+            break;
+        }
+        if (nums[i] === nums[i - 1]) {
+            i++;
+        }
+        let j = i + 1;
+        let k = nums.length - 1;
+
+        console.log("here");
+        while (j < k) {
+            const sum = nums[i] + nums[j] + nums[k];
+            if (sum === 0) {
+                result.push([nums[i] + nums[j] + nums[k]]);
+                j++;
+
+                k--;
+                while (j < k && nums[j] === nums[j - 1]) {
+                    j++;
+                }
+            } else if (sum > 0) {
+                j++;
+            } else {
+                k--;
+            }
+        }
+        i++;
+    }
+    return result;
 }
+
+const nums = [-1, 0, 1, 2, -1, -4];
+console.log(threeSum(nums));
