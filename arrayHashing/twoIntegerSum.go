37bb2e6d0098c374a8e7a9180ee768eb6f53047b
shuvoedward <38678545+shuvoedward@users.noreply.github.com>
2025-01-20 22:36:52 -0500

    written in go

---
+++ arrayHashing/twoIntegerSum.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+func twoSum(nums []int, target int) []int {
+	for i := 0; i<len(nums); i++{
+		for j:=i+1; j < len(nums); j++{
+
+			if nums[i] + nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}
+
+func twoPass(nums []int, target int) []int{
+	indices := make(map[int]int)
+
+	for i, n := range(nums){
+		indices[n] = i;
+	}
+
+	for i, n := range(nums){
+		diff := target - n;
+
+		if j, found := indices[diff]; found && j!= i {
+			return []int{i,j}
+		}
+	}
+
+	return []int{}
+}
+
+func main(){
+	nums := []int{3, 4, 5, 7}
+	target:= 7
+	
+	// result := twoSum(nums, target)
+	// fmt.Println(result)
+	fmt.Println(twoPass(nums, target))
+}
