d4ec3253edebc6838bca7844100a0f6b4ea01aa5
shuvoedward <38678545+shuvoedward@users.noreply.github.com>
2025-06-12 01:14:30 -0400

    count good nodes on binary tree

--- tree/goodNodes.go
+++ tree/goodNodes.go
@@ -1,1 +1,27 @@
 package tree
+
+func GoodNodes(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	var dfs func(node *TreeNode, maxVal int) int
+	dfs = func(node *TreeNode, maxVal int) int {
+		if node == nil {
+			return 0
+		}
+
+		res := 0
+		if node.Val >= maxVal {
+			res++
+		}
+
+		maxVal = max(node.Val, maxVal)
+		res += dfs(node.Left, maxVal)
+		res += dfs(node.Right, maxVal)
+
+		return res
+	}
+
+	return dfs(root, root.Val)
+}
